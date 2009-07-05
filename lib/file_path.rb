1fea48595eb785eb8a226c2c3737ae021187c595
Sergey Kruk <sergey.kruk@gmail.com>
2009-07-05 20:58:47 +0400

    changed current path behaviour, current returns path with filename, new returns current dirname

--- lib/file_path.rb
+++ lib/file_path.rb
@@ -1,16 +1,30 @@
 class FilePath < String
-  def initialize(path)
-    replace expand(path)
+  # Creates new file path. If path is not given, dirname of current file
+  # is taken
+  def initialize(path = nil)
+    if path
+      replace expand(path)
+    else
+      replace expand(File.dirname(caller(1)[0].split(":")[0]))
+    end
   end
+
+  # Joins string or filepath into new path
+  # Example:
+  #   FilePath.new("somedir")/"other_dir"/"file"     => "somedir/other_dir/file"
   def /(other)
     FilePath.new(expand(File.join(self, other)))
   end
+
   class << self
+    # Expanded path to current file: File.expand_path(__FILE__)
     def current
-      self.new(File.dirname(caller(1)[0].split(":")[0]))
+      new(caller(1)[0].split(":")[0])
     end
   end
+
 protected
+
   def expand(path)
     File.expand_path(path)
   end
--- spec/file_path_spec.rb
+++ spec/file_path_spec.rb
@@ -2,6 +2,10 @@ require File.join(File.dirname(__FILE__), "..", "lib", "file_path.rb")
 
 describe FilePath do
   it "should show correct current path" do
-    FilePath.current.should == File.expand_path(File.dirname(__FILE__))
+    FilePath.current.should == File.expand_path(__FILE__)
+  end
+
+  it "should show correct current dirname path when initialized" do
+    FilePath.new.should == File.expand_path(File.dirname(__FILE__))
   end
 end
